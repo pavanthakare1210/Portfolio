e57ed5966f0dff8bd74ac596f61a16d75f415d31
pavanthakare1210 <thakrepavan044@gmail.com>
2024-12-22 13:51:05 +0530

    first commit

---
+++ assets/script/port.js
@@ -0,0 +1,11 @@
+
+function visible() {
+     var click=document.getElementById('list')
+     if (click.style.display === "none") {
+        click.style.display = "block";  // Show the element
+    } else {
+        click.style.display = "none";   // Hide the element
+    }
+}
+
+
